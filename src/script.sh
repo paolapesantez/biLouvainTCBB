4632214fdab7b520f8434a10e173300b66427921
Paola Pesantez <p.pesantezcabrera@wsu.edu>
2017-01-23 23:01:18 -0800

    Update script.sh

--- src/script.sh
+++ src/script.sh
@@ -7,7 +7,7 @@ datasets=(SouthernWomen)
 for i in ${datasets[@];}
 do
 	fileName=$folder${i}"_bipartite.txt"
-	./$executable -i $fileName -d "\t" -merge 0
+	./$executable -i $fileName -d "\t" -fuse 1
 done
 exit 0
 
